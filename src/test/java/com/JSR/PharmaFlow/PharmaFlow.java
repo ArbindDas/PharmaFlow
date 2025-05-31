58cd6f9b33362c9d5511ad2c02ec15d6807f01f9
Arbind Das <dasarbind269@gmail.com>
2025-05-31 15:29:50 +0545

    Done

--- src/test/java/com/JSR/PharmaFlow/PharmaFlow.java
+++ src/test/java/com/JSR/PharmaFlow/PharmaFlow.java
@@ -1,16 +1,16 @@
-// package com.JSR.online_pharmacy_management;
-
+// package com.JSR.PharmaFlow;
+//
 // import org.junit.jupiter.api.Test;
 // import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 // import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 // import org.springframework.boot.test.context.SpringBootTest;
-
+//
 // @SpringBootTest
 // @EnableAutoConfiguration (exclude = DataSourceAutoConfiguration.class)
-// class OnlinePharmacyManagementApplicationTests {
-
+// class PharmaFlow {
+//
 // 	@Test
 // 	void contextLoads() {
 // 	}
-
+//
 // }
